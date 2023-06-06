3d4271c1c50e4d13f0f25b2a730202f7e4ad8453
unknown <barronpeterson56@gmail.com>
2023-06-05 19:08:28 -0500

    Add history and rest of functionality

--- assets/java-script/script.js
+++ assets/java-script/script.js
@@ -1,14 +1,62 @@
 var apiCode = "f0b6e0e48ee2f2d5d637188c67551b91";
 var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
-var apiForecast = "http://api.openweathermap.org/data/2.5/forecast?";
+var apiForecast = "http://api.openweathermap.org/data/2.5/forecast?lat=";
+var apiCurrent = "https://api.openweathermap.org/data/2.5/weather?lat="
 var displayResult = document.querySelector('#resultsArea');
+var historyContainer = document.querySelector("#historyBtn")
+
+var cityHistory = [];
+
+retrieveHistory()
+
+function retrieveHistory(){
+  var storedHistory = JSON.parse(localStorage.getItem('History'));
+  console.log(storedHistory)
+    if(storedHistory !== null){
+        cityHistory = storedHistory
+    }
+     historyContainer.innerHTML ="";
+    for(var i =0; i <cityHistory.length;i++){
+      var historyBtn = makeHistory(i);
+      historyContainer.appendChild(historyBtn);
+    }
+    }
+
+    function makeHistory(i) {
+      var itemEl = document.createElement('div');
+      itemEl.setAttribute('class', 'd-grid gap-2');
+      var btnEl = document.createElement('button');
+      btnEl.setAttribute('class', 'btn btn-primary');
+      btnEl.textContent = cityHistory[i];
+
+      btnEl.addEventListener('click', function() {
+        var clickedCity = cityHistory[i];
+        handleHistoryClick(clickedCity);
+      }); 
+      itemEl.append(btnEl);
+      return itemEl;
+    }
+    function handleHistoryClick(city) {
+      console.log('Clicked city:', city);
+      var locationGet = apiUrl + city + '&appid=' + apiCode;
+      getLocation(locationGet);
+    }
+    
 
 document.getElementById('currentButton').onclick =function(){
-  //var input = document.getElementById("search").value;
-  input = "Dallas";
+  var input = document.getElementById("search").value;
   var locationGet = apiUrl+input+'&appid='+apiCode;
   getLocation(locationGet);
+  cityHistory.push(input);
+  console.log(cityHistory);
+  localStorage.setItem("History",JSON.stringify(cityHistory))
+  retrieveHistory()
 }
+document.getElementById('clearHistory').onclick =function(){
+localStorage.clear();
+location.reload();
+}
+
 
 function getLocation(city){
   fetch(city)
@@ -19,6 +67,7 @@ function getLocation(city){
   .then(function(data){
     console.log(data);
     getCurrent(data);
+    getForecast(data);
   })
   .catch(function(error){
     console.log(error);
@@ -30,7 +79,7 @@ function getCurrent(current){
     console.log(cityLon);
   var cityLat = current[0].lat;
     console.log(cityLat);
-  var url = apiForecast+"lat="+cityLat+"&lon="+cityLon+"&appid="+apiCode+"&units=imperial"
+  var url = apiCurrent+cityLat+"&lon="+cityLon+"&appid="+apiCode+"&units=imperial"
   fetch(url)
   .then(function(res){
     if(!res.ok){throw new Error('Oh no!')}
@@ -45,26 +94,103 @@ function getCurrent(current){
   })
 }
 
-function createCurrent(cast){
-  displayResult.innerHTML = "";
-  var display = document.createElement('div');
-  display.classList.add("col-12");
-  var date = document.createElement('p')
-  date.textContent = cast.list[0].dt_txt
-  var title = document.createElement('h2');
-  title.textContent = cast.city.name;
-  var temp = document.createElement('p');
-  temp.textContent = "Temperature: "+ cast.list[0].main.temp + " °F"
-  var wind = document.createElement('p');
-  wind.textContent = "Wind Speed: "+ cast.list[0].wind.speed + " MPH"
-  var humid = document.createElement('p');
-  humid.textContent = "Humidity: "+ cast.list[0].main.humidity + "%"
-  title.append(date)
-  display.append(title,temp,wind,humid);
-  return display;
+function getForecast(forecast){
+  var cityLon = forecast[0].lon;
+    console.log(cityLon);
+  var cityLat = forecast[0].lat;
+    console.log(cityLat);
+    var url = apiForecast+cityLat+"&lon="+cityLon+"&appid="+apiCode+"&units=imperial"
+    fetch(url)
+  .then(function(res){
+    if(!res.ok){throw new Error('Oh no!')}
+    return res.json();
+  })
+  .then(function(dataForecast){
+    console.log(dataForecast);
+    displayForecast(dataForecast)
+  })
+  .catch(function(error){
+    console.log(error);
+  })
 }
 
 function displayCurrent(cast){
   var weather3h = createCurrent(cast);
   displayResult.appendChild(weather3h);
+}
+function displayForecast(cast){
+    for(var i = 1;i < 40 + 1;i += 8){
+    var fiveDay = createFiveDay(cast.list[i]);
+    displayResult.appendChild(fiveDay);
+    }
+}
+
+function createCurrent(cast){
+  displayResult.innerHTML = "";
+  var display = document.createElement('div');
+  var date = document.createElement('p')
+  var title = document.createElement('h2');
+  var temp = document.createElement('p');
+  var wind = document.createElement('p');
+  var humid = document.createElement('p');
+  var icon = document.createElement('img')
+  display.classList.add("col-12","gray");
+  date.textContent = "Currently"
+  title.textContent = cast.name;
+  temp.textContent = "Temperature: "+ cast.main.temp + " °F"
+  wind.textContent = "Wind Speed: "+ cast.wind.speed + " MPH"
+  humid.textContent = "Humidity: "+ cast.main.humidity + "%"
+  title.append(date)
+
+  if (cast.weather.main = "Clouds"){
+    icon.src="./assets/images/coudyIcon.png"
+    icon.style.width = "50px";
+    icon.style.height = "50px";
+  }
+  if (cast.weather.main = "Rain"){
+    icon.src="./assets/images/rainyIcon.png"
+    icon.style.width = "50px";
+    icon.style.height = "50px";
+  }
+  if (cast.weather.main = "Clear"){
+    icon.src="./assets/images/sunnyIcon.png"
+    icon.style.width = "50px";
+    icon.style.height = "50px"; 
+  }
+  display.append(title,icon,temp,wind,humid);
+  return display;
+}
+
+function createFiveDay(cast){
+  var display = document.createElement('div');
+  var date = document.createElement('p')
+  var title = document.createElement('h2');
+  var temp = document.createElement('p');
+  var wind = document.createElement('p');
+  var humid = document.createElement('p');
+  var icon = document.createElement('img')
+  display.classList.add("col-2","inline","gray");
+  date.textContent = cast.dt_txt
+  temp.textContent = "Temperature: "+ cast.main.temp + " °F"
+  wind.textContent = "Wind Speed: "+ cast.wind.speed + " MPH"
+  humid.textContent = "Humidity: "+ cast.main.humidity + "%"
+  title.append(date)
+  
+  if (cast.weather.main = "Clouds"){
+    icon.src="./assets/images/coudyIcon.png"
+    icon.style.width = "50px";
+    icon.style.height = "50px";
+  }
+  if (cast.weather.main = "Rain"){
+    icon.src="./assets/images/rainyIcon.png"
+    icon.style.width = "50px";
+    icon.style.height = "50px";
+  }
+  if (cast.weather.main = "Clear"){
+    icon.src="./assets/images/sunnyIcon.png"
+    icon.style.width = "50px";
+    icon.style.height = "50px"; 
+  }
+  display.append(title,icon,temp,wind,humid);
+return display
 }
